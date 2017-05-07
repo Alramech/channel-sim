0baae460286220dadea736dbce1639e6c8d28966
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2017-05-06 18:32:47 -0700

    Added SmartNode, which checks which channel is least congested to send on those channels. Also provides a sample override of send and receive

--- src/node.py
+++ src/node.py
@@ -61,6 +61,82 @@ class SinkNode(Node):
                 print "Delivered {} to sink, score = {}".format(self.token, self.score)
             self.readbuffer[i] = None
 
+class SmartNode(Node):
+    def __init__(self, pos):
+        Node.__init__(self, pos, "Smart")
+
+    # method to determine which channel to send for a particular neighbor
+    def channel_to_send_neighbor(self, neighbor):
+        ind = None
+        if neighbor.status == "REC":
+            ind = 0
+            while ind < neighbor.readbuffer.length and neighbor.readbuffer[ind] == None:
+                ind += 1
+            if ind == neighbor.readbuffer.length:
+                ind = None
+
+        return ind
+
+    # method to determine which channel to send in general (traffic)
+    def channel_to_send(self):
+        ind = None
+        smallest_num = float('inf')
+        smallest_num_index = -1
+        for ind in range(16):
+            channel_traffic = 0
+            for neighbor in self.neighbors:
+                if neighbor.status == "REC":
+                    if neighbor.readbuffer[ind] != None:
+                        channel_traffic += 1
+                else:
+                    if neighbor.sendbuffer[ind] != None:
+                        channel_traffic += 1
+            if channel_traffic < smallest_num:
+                smallest_num = channel_traffic
+                smallest_num_index = ind
+
+        return smallest_num_index
+
+
+    def send(self, msg):
+        # first determine which channel is least traffic congested
+        channel = self.channel_to_send()
+
+        # now set send buffer to the desired msg
+        self.sendbuffer[channel] = msg
+        for neighbor in self.neighbors:
+            if neighbor.status == "REC":
+                if neighbor.readbuffer[channel] != None:
+                    # TODO: what do we do here? just give up or try to send on another channel
+                    # for this specific neighbor?
+                    channel = channel_to_send_neighbor(neighbor)
+                neighbor.readbuffer[channel] = (msg, self)
+                print "{} at {} sending {} to {} at {} on channel {}".format(self.type, self.pos, msg, neighbor.type, neighbor.pos, channel)
+        
+        # Not sure about this?
+        self.nextStatus = "REC"
+
+    # in this case, dst is neighbor
+    def send(self, msg, dst):
+        channel = channel_to_send_neighbor(dst)
+        neighbor.readbuffer[channel] = (msg, self)
+        print "{} at {} sending {} to {} at {} on channel {}".format(self.type, self.pos, msg, neighbor.type, neighbor.pos, channel)
+        self.nextStatus = "REC"
+
+    def recieve(self):
+        # listen to all channels
+        for i, item in enumerate(self.readbuffer):
+            if item == None:
+                continue
+            print "{} at {} recieved {} from {} at {} on channel {}".format(self.type, self.pos, item[0], item[1].type, item[1].pos, i)
+            self.readbuffer[i] = None
+            item[1].sendbuffer[i] = None
+
+        # Not sure about this?
+        if not all(x is None for x in self.sendbuffer):
+            self.nextStatus = "SEND"
+
+
 class HotPotatoNode(Node):
     def __init__(self, pos):
         Node.__init__(self, pos, "HP")
